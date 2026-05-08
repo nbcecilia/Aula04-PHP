44e3f875fcb393cb2ee1a96fb333f3b1c69e0129
Cecilia Nunes <nbricecilia@gmail.com>
2026-05-08 19:33:39 -0300

    iniciado2

--- ex02.php
+++ ex02.php
@@ -1,1 +1,58 @@
 <!-- 02 – CRIE UMA CALCULADOR COM SWITCH CASE; -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Aula 04 - PHP</title>
+
+    <style>
+        body {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h3>Questão 02</h3>
+    <form method = "POST">
+        
+        <label> Digite o 1º número:</label><br><br>
+        <input type="number" name="n1" step="1" required><br><br>
+        <label> Digite o 2º número:</label><br><br>
+        <input type="number" name="n2" step="1" required><br><br>
+        <label> Selecione a operação:</label><br>
+        <input type="radio" id="multi" name="op" value="multi" /> Multiplicação <br>
+        <input type="radio" id="op" name="op" value="soma" /> Soma<br>
+        <input type="radio" id="op" name="op" value="sub" /> Subtração <br>
+        <input type="radio" id="op" name="op" value="divi" /> Divisão <br><br><br>
+        <button type="submit">Calcular</button><br><br>
+    </form>
+    <hr>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $n1= $_POST["n1"] ?? 0;
+        $n2= $_POST["n2"] ?? 0;
+        $op= $_POST["op"] ?? 0;
+
+        switch($op){
+            case "multi":
+                $total = $n1 * $n2; 
+                echo "<br><h3>O valor da multiplicação é: $total ";
+                break;
+            case "soma":
+                $total2 = $n1 + $n2;
+                echo " <br><h3>O valor da soma é: $total2";
+                break;
+            case "sub":
+                $total3 = $n1 - $n2;  
+                echo "<br><h3>O valor da subtração é: $total3 ";
+                break;
+            case "divi": 
+                $total4 = $n1 / $n2;
+                echo "<br><h3>O valor da divisão é: $total4";
+                break;
+            default: echo "<h3>Opção Inválida";
+        }
+    }
+    ?> 
+</body>
+</html>
--- ex03.php
+++ ex03.php
@@ -1,3 +1,6 @@
+<!-- 03 – CRIE UM PROGRAMA QUE RECEBA UM NÚMERO DE 1 A 7 E DEPOIS UM NÚMERO DE 1 A
+12 E INFORME, RESPECTIVAMENTE, O DIA DA SEMANA E O MÊS CORRESPONDENTE À ESSES
+NÚMEROS -->
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -5,27 +8,31 @@
     <title>Aula 04 - PHP</title>
 </head>
 <body>
+    <h3>Questão 03</h3>
     <form method = "POST">
-        <label> Digite um número de 1 a 4:</label><br><br>
+        <label> Digite um número de 1 a 7:</label><br><br>
+        <input type="number" name="num" step="1" required><br><br>
+        <label> Digite um número de 1 a 12:</label><br><br>
         <input type="number" name="num" step="1" required><br><br>
         <button type="submit">Responder</button><br><br>
     </form>
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        
         $num= $_POST["num"] ?? 0;
         switch($num){
             case 1: 
-                echo "Primavera";
+                echo "";
                 break;
             case 2: 
-                echo "Verão";
+                echo "";
                 break;
             case 3: 
-                echo "Outono";
+                echo "";
                 break;
-            case 4: echo "Inverno";
+            case 4: echo "";
                 break;
-            default: echo "Estação inválida";
+            default: echo "";
         }
     }
     ?> 
