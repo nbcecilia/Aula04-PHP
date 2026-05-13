c3fd5fea97d6096f62d8ac535c811e64dd7dfb34
AlunoETC <alunoetc@EDUC.NET>
2026-05-13 19:54:24 -0300

    resolvido

--- ex04.php
+++ ex04.php
@@ -11,7 +11,7 @@ RECUPERAÇÃO; DE 7 A 8, APROVADO;  -->
 <body>
     <h3>Questão 4</h3>
     <form method="POST">
-     <label>Digite a 1ª nota: </label>
+    <label>Digite a 1ª nota: </label>
     <input type="number" name="n1" step ="0.1"required><br><br>
     <label>Digite a 2ª nota: </label>
     <input type="number" name="n2" step ="0.1" required><br><br>
@@ -19,37 +19,31 @@ RECUPERAÇÃO; DE 7 A 8, APROVADO;  -->
     <input type="number" name="n3"  step ="0.1" required><br><br>
     <label>Digite a 4ª nota: </label>
     <input type="number" name="n4" step ="0.1" required><br><br>
-    <button type="submit">Responder</button>
+    <input type="submit" value="CALCULAR">
     </form>
     
 <hr>
-    <?php
+<?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $n1= $_POST["n1"] ?? 0;
         $n2= $_POST["n2"] ?? 0;
         $n3= $_POST["n3"] ?? 0;
         $n4= $_POST["n4"] ?? 0;
- $notafinal= ($n1 + $n2 + $n3 + $n4)/4;
-        switch ($notafinal) {
-            case '1':
-                $notafinal >= 1 && $notafinal <= 4;
-                echo"Nota final $notafinal, situação do aluno: Reprovado";
-                break;
-            case '2':
-                $notafinal >= 5 && $notafinal <= 6;
-                echo"Nota final $notafinal, situação do aluno: Recuperação";
-                break;
-            case '3':
-                $notafinal >= 7 && $notafinal <= 8;
-                echo"Nota final $notafinal, situação do aluno: Aprovado";
-                break;
-            
-            default:
-                echo"Nota inválida";
-                break;
-        }
-    }
-    ?>
+$notafinal= ($n1 + $n2 + $n3 + $n4)/4;
+
+if($notafinal < 5){
+    echo"<h3>Nota final $notafinal, aluno REPROVADO</h3>"; 
+}
+elseif ($notafinal >= 5 && $notafinal <= 7) {
+    echo"<h3>Nota final $notafinal, aluno em Recuperação</h3>";
+}
+elseif ($notafinal >= 7 && $notafinal <= 10) {
+    echo"<h3>Nota final $notafinal, aluno APROVADO</h3>";
+}else {
+    echo "<h3>Erro desconhecido</h3>";
+}
+}
+?>
 
 </body>
 </html>
